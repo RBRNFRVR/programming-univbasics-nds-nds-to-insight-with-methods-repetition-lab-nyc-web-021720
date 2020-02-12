4d984007a928608641f98a4da1b2d6bd94ac064e
Joseph Chavez <rommelchavez99@gmail.com>
2020-02-12 22:36:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,13 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+   director_names = []
+  index = 0
+  while index < source.length do
+    director_names = director_names << source[index][:name]
+    index +=1
+  end
+  director_names
 end
 
 def total_gross(source)
